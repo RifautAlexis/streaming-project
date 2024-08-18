158c61c6e98f2bde5739fabe3a0ac891ec9980f4
Rifaut Alexis <ralexis149@gmail.com>
2024-08-18 18:03:14 +0200

    add basic features in header

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -3,12 +3,23 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.less',
-  imports: [NzLayoutModule, RouterModule, NzMenuModule, NgOptimizedImage],
+  imports: [
+    NzLayoutModule,
+    RouterModule,
+    NzMenuModule,
+    NgOptimizedImage,
+    NzAvatarModule,
+    NzButtonModule,
+    NzDropDownModule,
+  ],
 })
 export class AppComponent {}
--- apps/frontend/src/app/app.config.ts
+++ apps/frontend/src/app/app.config.ts
@@ -4,11 +4,15 @@ import {
   APP_INITIALIZER,
   FactoryProvider,
 } from '@angular/core';
+import { provideAnimations } from "@angular/platform-browser/animations";
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { ThemeService } from './services/theme.service';
+import { provideNzIcons } from "ng-zorro-antd/icon";
+import { IconDefinition } from "@ant-design/icons-angular";
+import { UserOutline } from "@ant-design/icons-angular/icons";
 
-export const AppInitializersProvider: FactoryProvider[] = [
+const AppInitializersProvider: FactoryProvider[] = [
   {
     provide: APP_INITIALIZER,
     useFactory: (themeService: ThemeService) => async () => {
@@ -19,10 +23,16 @@ export const AppInitializersProvider: FactoryProvider[] = [
   },
 ];
 
+const icons: IconDefinition[] = [
+  UserOutline
+];
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideAnimations(),
     provideRouter(appRoutes),
+    provideNzIcons(icons),
     ...AppInitializersProvider,
   ],
 };
