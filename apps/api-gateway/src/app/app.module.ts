dd52d61ab4cd1a3886cc570a8a649c2262096cf9
Rifaut Alexis <ralexis149@gmail.com>
2024-08-24 17:39:35 +0200

    add authentication

--- apps/api-gateway/src/app/app.module.ts
+++ apps/api-gateway/src/app/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthenticationModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthenticationModule, UsersModule],
 })
 export class AppModule {}
