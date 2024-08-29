254f9ff86b5c61fc61628e0e103522260d73b987
Rifaut Alexis <ralexis149@gmail.com>
2024-08-29 21:28:06 +0200

    add YARP with authentication

--- src/server/ApiGateway/Program.cs
+++ src/server/ApiGateway/Program.cs
@@ -1,6 +1,23 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("ApiGatewayPolicy", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+    });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapReverseProxy();
 
 app.Run();
--- src/server/ApiGateway/obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
+++ src/server/ApiGateway/obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+727a28bf280f299b71e4a722b0a7e3d8722930c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebfb52db1991866c58105c1ad2674a5112474bc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
